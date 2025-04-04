6da817ab25e03857060d9aa75c3a9bd8009dc8b3
dnrsz <dnrsz@example.com>
2025-04-04 00:03:49 -0600

    add github pages support astrojs

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://www.andreschipol.com',
+  base: 'web',
   vite: {
     plugins: [tailwindcss()]
   }
